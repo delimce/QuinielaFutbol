52c56f62465d86e517c8efeefffaa04a9f258304
Luis De Lima <luis.delima@kavak.com>
2022-08-02 00:43:49 +0200

    fix: save user register bug

--- app/controllers/registro/save.php
+++ app/controllers/registro/save.php
@@ -7,10 +7,16 @@ function _save() {
 
     $pass = Form::getVar("clave");
     $_POST["r0clave"] = md5($pass);
-    $db->dataInsert("r", "0", "usuario", $_POST);
+
+    $db->setTable("usuario");
+    $db->setField("nombre", $_POST["r0nombre"]);
+    $db->setField("email", $_POST["r0email"]);
+    $db->setField("usuario", $_POST["r0usuario"]);
+    $db->setField("clave", $_POST["r0clave"]);
+    $db->setField("profile", "user");
+    $db->setField("contacto", $_POST["r0contacto"]);
+    $db->insertInTo();
 
     $db->close();
     echo '<h4 class="alert_success">Registro Exitoso</h4>';
 }
-
-?>
